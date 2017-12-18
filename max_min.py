028e66d5adaed3955f6c53756a1abb326a734db3
robintiwari07 <robin.nate22@gmail.com>
2017-12-18 10:08:21 +0545

    Finding the maximum and minimum number in a list

---
+++ max_min.py
@@ -0,0 +1,11 @@
+my_list = [4, 2, 4, 0, 2, 4, 5, 7, 8, 9, 23, 8, 5, 4, 2, 2, 34, 4, 45]
+
+my_list.sort()
+print(my_list)
+
+min_number= my_list[0]
+print("Minimum Number in the list is: ",min_number)
+
+max_number= my_list[len(my_list)-1]
+print("Maximum number in the list is: ",max_number)
+
