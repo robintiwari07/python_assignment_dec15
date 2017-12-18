f97b71d232e97fd5bc645863355cde5ada9ac16c
robintiwari07 <robin.nate22@gmail.com>
2017-12-18 10:04:39 +0545

    Solving a linear equation and printing the value of y for each iteration.

---
+++ linear_equation.py
@@ -0,0 +1,9 @@
+def solve_for_y(x_values):
+    m = 45
+    c = 0.5
+    for x in x_values:
+        y= m *x +c
+        print("the required value of y is: {0:.2f}".format(y))
+
+x_values=[1, 2.3, 5.6, 7, 78]
+solve_for_y(x_values)
